7693b2e5bcd2788a2e20778fc80a58e7c2dd6910
Sven Nijhuis <sanijhuis@live.nl>
2023-01-19 21:22:18 +0100

    wip

--- src/components/layout/main-filter.jsx
+++ src/components/layout/main-filter.jsx
@@ -16,19 +16,19 @@ function MainFilter() {
   const removeData = async () => {
     const data = deleteMessagesId;
 
-    await fetch(
-      "https://gmail.googleapis.com/gmail/v1/users/me/messages/batchDelete",
-      {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + Cookies.get("keyFetch"),
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ids: data,
-        }),
-      }
-    );
+    // await fetch(
+    //   "https://gmail.googleapis.com/gmail/v1/users/me/messages/batchDelete",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       Authorization: "Bearer " + Cookies.get("keyFetch"),
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       ids: data,
+    //     }),
+    //   }
+    // );
     setParty("party");
   };
 
