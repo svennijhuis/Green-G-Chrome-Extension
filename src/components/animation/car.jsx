f7061b79152749c5bde964ae80175e66caf80b25
Sven Nijhuis <sanijhuis@live.nl>
2023-01-22 23:26:25 +0100

    animation

--- src/components/animation/car.jsx
+++ src/components/animation/car.jsx
@@ -1,11 +1,30 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-import car from "./drivingcar.json";
+import car from "./standalonecar.json";
+import smoke from "./smokecloud.json";
 import road from "./road.json";
+import { useDataContext } from "../../context/data";
+import { useEffect, useState } from "react";
 
 function CarAnimation() {
+  const { dataMessages } = useDataContext();
+
+  const [height, setHeight] = useState(60);
+
+  useEffect(() => {
+    if (dataMessages.children) {
+      setHeight(dataMessages.children.length);
+    }
+  }, [dataMessages]);
   return (
     <>
       <div className="mt-auto">
+        <Player
+          src={smoke}
+          className="player absolute bottom-[5px] left-10"
+          loop
+          autoplay
+          style={{ height: `${height / 2}px`, maxHeight: "150px" }}
+        />
         <Player
           src={car}
           className="player-car"
